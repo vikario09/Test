d83bee3af4318d3ee0e33d964caa374006789c5f
Vika Kamenkova <kamenkova09@gmail.com>
2024-12-12 20:47:07 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://vikario.space/test-approve-leads/',
+  base: '/test-approve-leads',
+});
